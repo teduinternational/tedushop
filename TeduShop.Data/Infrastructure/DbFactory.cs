613e4ff09f605dba3533d9a7092fe9a4c3b47da6
ToanBN <tedu.international@gmail.com>
2016-05-04 08:11:36 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext dbContext;
+
+        public TeduShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new TeduShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
