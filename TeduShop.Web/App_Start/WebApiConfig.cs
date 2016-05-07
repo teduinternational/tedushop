3b8750f40c22736fa4a80db470e501fb8afccdb4
ToanBN <tedu.international@gmail.com>
2016-05-07 08:34:02 +0700

    how to use web api

--- TeduShop.Web/App_Start/WebApiConfig.cs
+++ TeduShop.Web/App_Start/WebApiConfig.cs
@@ -18,6 +18,7 @@ namespace TeduShop.Web
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+                
             );
         }
     }
