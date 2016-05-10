10a645478f268e173ba4cf37394e968b43386bd1
ToanBN <tedu.international@gmail.com>
2016-05-10 07:19:00 +0700

    session 17: how to declare module and controller

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller("myController", myController);
+
+myController.$inject = ['$scope'];
+//declare
+function myController($scope) {
+    $scope.message = "This is my message from Controller";
+}
