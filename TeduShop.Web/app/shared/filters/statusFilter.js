684311316e8021978da2c4e6dce4cfdec8c701e0
ToanBN <tedu.international@gmail.com>
2016-05-12 21:34:14 +0700

    add filter status and format data in list

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('tedushop.common'));
