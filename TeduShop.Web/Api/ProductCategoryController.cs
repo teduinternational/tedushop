29919c4a6972e74dad1b736d7ebd5ca11f538d00
ToanBN <tedu.international@gmail.com>
2016-05-13 07:05:39 +0700

    paging for list

--- TeduShop.Web/Api/ProductCategoryController.cs
+++ TeduShop.Web/Api/ProductCategoryController.cs
@@ -15,7 +15,7 @@ namespace TeduShop.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
+        private IProductCategoryService _productCategoryService;
 
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
             : base(errorService)
@@ -24,13 +24,26 @@ namespace TeduShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
--- TeduShop.Web/app/components/product_categories/productCategoryListController.js
+++ TeduShop.Web/app/components/product_categories/productCategoryListController.js
@@ -5,12 +5,23 @@
 
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
-
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCagories = getProductCagories;
 
-        function getProductCagories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        function getProductCagories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    page: page,
+                    pageSize: 20
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
                 console.log('Load productcategory failed.');
             });
