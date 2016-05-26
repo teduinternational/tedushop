7f574cef3edb8856229224739a0b3f87352dc85b
ToanBN <tedu.international@gmail.com>
2016-05-26 07:26:14 +0700

    add client layouts resources

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -26,5 +26,23 @@ namespace TeduShop.Web.Controllers
 
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
     }
 }
