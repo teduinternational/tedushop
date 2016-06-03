71afea1d20b811ece3a6222ef9dd51359d171e82
ToanBN <tedu.international@gmail.com>
2016-06-03 07:08:03 +0700

    output cache for website

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace TeduShop.Web.Controllers
     {
         IProductCategoryService _productCategoryService;
         IProductService _productService;
-        ICommonService  _commonService;
+        ICommonService _commonService;
 
         public HomeController(IProductCategoryService productCategoryService,
             IProductService productService,
@@ -25,10 +25,11 @@ namespace TeduShop.Web.Controllers
             _productService = productService;
         }
 
+        [OutputCache(Duration = 60, Location = System.Web.UI.OutputCacheLocation.Server)]
         public ActionResult Index()
         {
             var slideModel = _commonService.GetSlides();
-            var slideView = Mapper.Map<IEnumerable<Slide>,IEnumerable<SlideViewModel>>(slideModel);
+            var slideView = Mapper.Map<IEnumerable<Slide>, IEnumerable<SlideViewModel>>(slideModel);
             var homeViewModel = new HomeViewModel();
             homeViewModel.Slides = slideView;
 
@@ -41,21 +42,9 @@ namespace TeduShop.Web.Controllers
             return View(homeViewModel);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Footer()
         {
             var footerModel = _commonService.GetFooter();
@@ -70,10 +59,11 @@ namespace TeduShop.Web.Controllers
         }
 
         [ChildActionOnly]
+        [OutputCache(Duration = 3600)]
         public ActionResult Category()
         {
             var model = _productCategoryService.GetAll();
-            var listProductCategoryViewModel = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>> (model);
+            var listProductCategoryViewModel = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
             return PartialView(listProductCategoryViewModel);
         }
     }
