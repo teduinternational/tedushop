22a5edea4e72e2fffea6b56459e8b771a27d8320
ngoct <ngoct@DESKTOP-1GBIUOK>
2017-05-30 22:15:10 +0700

    fix bug auto cosllapse menu

--- TeduShop.Web/Assets/admin/js/app.js
+++ TeduShop.Web/Assets/admin/js/app.js
@@ -388,7 +388,7 @@ function _init() {
   $.AdminLTE.tree = function (menu) {
     var _this = this;
     var animationSpeed = $.AdminLTE.options.animationSpeed;
-    $(document).on('click', menu + ' li a', function (e) {
+    $(document).off('click').on('click', menu + ' li a', function (e) {
       //Get the clicked link and the next element
       var $this = $(this);
       var checkElement = $this.next();
@@ -451,7 +451,7 @@ function _init() {
       var btn = $(o.toggleBtnSelector);
 
       //Listen to the click event
-      btn.on('click', function (e) {
+      btn.off('click').on('click', function (e) {
         e.preventDefault();
         //If the sidebar is not open
         if (!sidebar.hasClass('control-sidebar-open')
